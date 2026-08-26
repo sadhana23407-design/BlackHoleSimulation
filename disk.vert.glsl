66ec0bc8c6d9e66b774d8404faaa562003ee7daa
Sadhana S <sadhana23407@gmail.com>
2026-08-26 20:21:18 +0530

    Update disk.vert.glsl

--- disk.vert.glsl
+++ disk.vert.glsl
@@ -1,15 +1,21 @@
 #version 330 core
 
-layout(location = 0) in vec3 aPos;
-layout(location = 1) in vec3 aColor;
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
 
 out vec3 particleColor;
 
+uniform mat4 u_view;
+uniform mat4 u_projection;
+
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
+    // Transform object 3D position to screen space
+    gl_Position = u_projection * u_view * vec4(aPos, 1.0);
 
-    gl_PointSize = 2.5;
+    // Scale point size dynamically based on distance to camera
+    float dist = gl_Position.w;
+    gl_PointSize = clamp(100.0 / dist, 3.0, 20.0);
 
     particleColor = aColor;
 }
